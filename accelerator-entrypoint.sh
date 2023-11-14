6cb2261c4c2ed50d7f48d999bd1aede30a6e71a3
Qixiong Yuan <thisdk@gmail.com>
2023-11-14 11:24:58 +0800

    Update accelerator-entrypoint.sh

--- accelerator-entrypoint.sh
+++ accelerator-entrypoint.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-if [ "$KCPTUBE_ADDRESS" ];then
-    sed -i "s#kcptube_address_value#$KCPTUBE_ADDRESS#g" /etc/kcptube/config.conf
-    sed -i "s#singbox-address-value#$KCPTUBE_ADDRESS#g" /etc/sing-box/config.json
-else
-    sed -i "s#kcptube_address_value#0.0.0.0#g" /etc/kcptube/config.conf
-    sed -i "s#singbox-address-value#127.0.0.1#g" /etc/sing-box/config.json
-fi
-
 if [ "$KCPTUBE_PORT_START" ] && [ "$KCPTUBE_PORT_END" ]; then
     sed -i "s#kcptube_port_start#$KCPTUBE_PORT_START#g" /etc/kcptube/config.conf
     sed -i "s#kcptube_port_end#$KCPTUBE_PORT_END#g" /etc/kcptube/config.conf
 else
-    sed -i "s#kcptube_port_start#55000#g" /etc/kcptube/config.conf
-    sed -i "s#kcptube_port_end#65000#g" /etc/kcptube/config.conf
+    sed -i "s#kcptube_port_start#60000#g" /etc/kcptube/config.conf
+    sed -i "s#kcptube_port_end#62500#g" /etc/kcptube/config.conf
 fi
 
 exec "$@"
