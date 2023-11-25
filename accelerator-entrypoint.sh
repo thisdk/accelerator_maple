716f01f437d83de605535f8f878eb25163efdf68
Qixiong Yuan <thisdk@gmail.com>
2023-11-25 23:36:51 +0800

    Update accelerator-entrypoint.sh

--- accelerator-entrypoint.sh
+++ accelerator-entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-if [ "$KCPTUBE_PORT_START" ] && [ "$KCPTUBE_PORT_END" ]; then
-    sed -i "s#kcptube_port_start#$KCPTUBE_PORT_START#g" /etc/kcptube/config.conf
-    sed -i "s#kcptube_port_end#$KCPTUBE_PORT_END#g" /etc/kcptube/config.conf
+if [ "$KCPTUBE_PORT" ]; then
+    sed -i "s#kcptube_port#$KCPTUBE_PORT#g" /etc/kcptube/config.conf
 else
-    sed -i "s#kcptube_port_start#60000#g" /etc/kcptube/config.conf
-    sed -i "s#kcptube_port_end#62500#g" /etc/kcptube/config.conf
+    sed -i "s#kcptube_port#8585#g" /etc/kcptube/config.conf
 fi
 
 exec "$@"
